51a8e9d6e197f0472e17172c756a741341d753c8
Simplice NIYONZIMA <89002414+Simplice24@users.noreply.github.com>
2023-01-30 06:09:38 -0800

    Add files via upload

---
+++ couriersystem.sql
@@ -0,0 +1,304 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 30, 2023 at 03:08 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `couriersystem`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `branch`
+--
+
+CREATE TABLE `branch` (
+  `branch_id` int(11) NOT NULL,
+  `branch_name` varchar(60) NOT NULL,
+  `created_at` date NOT NULL,
+  `created_by` varchar(60) NOT NULL,
+  `updated_at` date NOT NULL,
+  `updated_by` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `branch`
+--
+
+INSERT INTO `branch` (`branch_id`, `branch_name`, `created_at`, `created_by`, `updated_at`, `updated_by`) VALUES
+(1, 'Nyabugogo', '2023-01-30', 'Simplice', '2023-01-30', 'Simplice');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+
+CREATE TABLE `customer` (
+  `customer_id` int(11) NOT NULL,
+  `fullname` varchar(60) NOT NULL,
+  `subscription` varchar(10) NOT NULL,
+  `idn` int(11) NOT NULL,
+  `telephone` varchar(15) NOT NULL,
+  `created_at` date NOT NULL,
+  `created_by` varchar(60) NOT NULL,
+  `updated_at` date NOT NULL,
+  `updated_by` varchar(60) NOT NULL,
+  `item_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `item`
+--
+
+CREATE TABLE `item` (
+  `item_id` int(11) NOT NULL,
+  `item_name` varchar(60) NOT NULL,
+  `value` int(11) NOT NULL,
+  `sender_name` varchar(60) NOT NULL,
+  `sender_phone` varchar(15) NOT NULL,
+  `sender_subscription` varchar(10) NOT NULL,
+  `receiver_name` varchar(60) NOT NULL,
+  `receiver_phone` varchar(15) NOT NULL,
+  `receiver_id` int(11) NOT NULL,
+  `departure` varchar(60) NOT NULL,
+  `depature_date` date NOT NULL,
+  `departure_time` date NOT NULL,
+  `destination` varchar(60) NOT NULL,
+  `created_at` date NOT NULL,
+  `created_by` varchar(60) NOT NULL,
+  `updated_at` date NOT NULL,
+  `updated_by` varchar(60) NOT NULL,
+  `manifest_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `log`
+--
+
+CREATE TABLE `log` (
+  `log_id` int(11) NOT NULL,
+  `field_name` varchar(60) NOT NULL,
+  `old_value` varchar(60) NOT NULL,
+  `new_value` varchar(60) NOT NULL,
+  `created_at` date NOT NULL,
+  `created_by` varchar(60) NOT NULL,
+  `updated_at` date NOT NULL,
+  `updated_by` varchar(60) NOT NULL,
+  `item_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `manifest`
+--
+
+CREATE TABLE `manifest` (
+  `manifest_id` int(11) NOT NULL,
+  `departure_date` date NOT NULL,
+  `departure_time` date NOT NULL,
+  `plate_number` varchar(15) NOT NULL,
+  `driver` varchar(60) NOT NULL,
+  `created_at` date NOT NULL,
+  `created_by` varchar(60) NOT NULL,
+  `updated_at` date NOT NULL,
+  `updated_by` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `status`
+--
+
+CREATE TABLE `status` (
+  `status_id` int(11) NOT NULL,
+  `status_value` int(11) NOT NULL,
+  `created_at` date NOT NULL,
+  `created_by` varchar(60) NOT NULL,
+  `updated_at` date NOT NULL,
+  `updated_by` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subscription`
+--
+
+CREATE TABLE `subscription` (
+  `subscription_id` int(11) NOT NULL,
+  `subscription_type` varchar(60) NOT NULL,
+  `created_at` date NOT NULL,
+  `created_by` varchar(60) NOT NULL,
+  `updated_at` date NOT NULL,
+  `updated_by` varchar(60) NOT NULL,
+  `customer_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subscription_type`
+--
+
+CREATE TABLE `subscription_type` (
+  `id` int(11) NOT NULL,
+  `name` varchar(60) NOT NULL,
+  `created_at` date NOT NULL,
+  `payment_way` varchar(60) NOT NULL,
+  `created_by` varchar(60) NOT NULL,
+  `updated_by` varchar(60) NOT NULL,
+  `updated_at` date NOT NULL,
+  `subscription_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL,
+  `user_fullname` varchar(60) NOT NULL,
+  `email` varchar(40) NOT NULL,
+  `telephone` varchar(15) NOT NULL,
+  `password` varchar(40) NOT NULL,
+  `created_at` date NOT NULL,
+  `created_by` varchar(60) NOT NULL,
+  `updated_at` date NOT NULL,
+  `updated_by` varchar(60) NOT NULL,
+  `role` varchar(225) NOT NULL,
+  `branch_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `branch`
+--
+ALTER TABLE `branch`
+  ADD PRIMARY KEY (`branch_id`);
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`customer_id`),
+  ADD UNIQUE KEY `idn` (`idn`),
+  ADD KEY `item_id` (`item_id`);
+
+--
+-- Indexes for table `item`
+--
+ALTER TABLE `item`
+  ADD PRIMARY KEY (`item_id`),
+  ADD KEY `manifest_id` (`manifest_id`);
+
+--
+-- Indexes for table `log`
+--
+ALTER TABLE `log`
+  ADD PRIMARY KEY (`log_id`),
+  ADD UNIQUE KEY `item_id` (`item_id`);
+
+--
+-- Indexes for table `manifest`
+--
+ALTER TABLE `manifest`
+  ADD PRIMARY KEY (`manifest_id`);
+
+--
+-- Indexes for table `status`
+--
+ALTER TABLE `status`
+  ADD PRIMARY KEY (`status_id`);
+
+--
+-- Indexes for table `subscription`
+--
+ALTER TABLE `subscription`
+  ADD PRIMARY KEY (`subscription_id`),
+  ADD KEY `customer_id` (`customer_id`);
+
+--
+-- Indexes for table `subscription_type`
+--
+ALTER TABLE `subscription_type`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `subscription_id` (`subscription_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`),
+  ADD KEY `branch_id` (`branch_id`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `customer`
+--
+ALTER TABLE `customer`
+  ADD CONSTRAINT `customer_ibfk_1` FOREIGN KEY (`item_id`) REFERENCES `item` (`item_id`);
+
+--
+-- Constraints for table `item`
+--
+ALTER TABLE `item`
+  ADD CONSTRAINT `item_ibfk_1` FOREIGN KEY (`manifest_id`) REFERENCES `manifest` (`manifest_id`);
+
+--
+-- Constraints for table `log`
+--
+ALTER TABLE `log`
+  ADD CONSTRAINT `log_ibfk_1` FOREIGN KEY (`item_id`) REFERENCES `item` (`item_id`);
+
+--
+-- Constraints for table `subscription`
+--
+ALTER TABLE `subscription`
+  ADD CONSTRAINT `subscription_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`customer_id`);
+
+--
+-- Constraints for table `subscription_type`
+--
+ALTER TABLE `subscription_type`
+  ADD CONSTRAINT `subscription_type_ibfk_1` FOREIGN KEY (`subscription_id`) REFERENCES `subscription` (`subscription_id`);
+
+--
+-- Constraints for table `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`branch_id`) REFERENCES `branch` (`branch_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
