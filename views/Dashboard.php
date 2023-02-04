3117bd38631cea5b9963783985dbf885642f9c45
Simplice NIYONZIMA <89002414+Simplice24@users.noreply.github.com>
2023-02-04 09:42:36 -0800

    Layout Customization

---
+++ views/Dashboard.php
@@ -0,0 +1,15 @@
+<?php
+
+use app\models\Branch;
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\grid\ActionColumn;
+use yii\grid\GridView;
+
+/** @var yii\web\View $this */
+/** @var app\models\BranchSearch $searchModel */
+/** @var yii\data\ActiveDataProvider $dataProvider */
+
+$this->title = 'Branches';
+$this->params['breadcrumbs'][] = $this->title;
+?>
