bea6de5bed9ae6a30c9714ba7419ea9db7bbf4c7
Simplice NIYONZIMA <89002414+Simplice24@users.noreply.github.com>
2023-02-04 10:37:26 -0800

    Layout Customization

--- views/status/update.php
+++ views/status/update.php
@@ -5,7 +5,7 @@ use yii\helpers\Html;
 /** @var yii\web\View $this */
 /** @var app\models\Status $model */
 
-$this->title = 'Update Status: ' . $model->status_id;
+// $this->title = 'Update Status: ' . $model->status_id;
 $this->params['breadcrumbs'][] = ['label' => 'Statuses', 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->status_id, 'url' => ['view', 'status_id' => $model->status_id]];
 $this->params['breadcrumbs'][] = 'Update';
