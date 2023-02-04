938b639692db256a7f8d10ae12ffbe0793235d47
Simplice NIYONZIMA <89002414+Simplice24@users.noreply.github.com>
2023-02-04 10:42:44 -0800

    Layout Customization

--- views/user/update.php
+++ views/user/update.php
@@ -5,7 +5,7 @@ use yii\helpers\Html;
 /** @var yii\web\View $this */
 /** @var app\models\User $model */
 
-$this->title = 'Update User: ' . $model->user_id;
+// $this->title = 'Update User: ' . $model->user_id;
 $this->params['breadcrumbs'][] = ['label' => 'Users', 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->user_id, 'url' => ['view', 'user_id' => $model->user_id]];
 $this->params['breadcrumbs'][] = 'Update';
