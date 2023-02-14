f7161b19136cfc264a90572315b0dc585caf53d9
Simplice NIYONZIMA <89002414+Simplice24@users.noreply.github.com>
2023-02-13 22:48:55 -0800

    first commit

--- config/web.php
+++ config/web.php
@@ -42,6 +42,11 @@ $config = [
             ],
         ],
         'db' => $db,
+        'authManager' => [
+            'class' => 'yii\rbac\DbManager',
+            // uncomment if you want to cache RBAC items hierarchy
+            // 'cache' => 'cache',
+        ],
         /*
         'urlManager' => [
             'enablePrettyUrl' => true,
