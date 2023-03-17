85e433ff3c4c5150fadd9d5651eea3959a6e101c
Simplice NIYONZIMA <89002414+Simplice24@users.noreply.github.com>
2023-03-17 03:03:53 -0800

    first commit

--- views/item/pdf_view.php
+++ views/item/pdf_view.php
@@ -3,58 +3,69 @@
 use yii\helpers\Html;
 use yii\grid\GridView;
 
-$this->title ='Items';
+$this->title ='Items report';
 $this->params['breadcrumbs'][]= $this->title;
 ?>
 
 <div class="container">
 <h2><?= Html::encode($this->title)?></h2>
-<div class="col-lg-12 grid-margin stretch-card">
-                <div class="card">
-                  <div class="card-body">
-                    <h4 class="card-title">Striped Table</h4>
-                    <p class="card-description"> Add class <code>.table-striped</code>
-                    </p>
-                    <table class="table table-striped">
-                      <thead>
-                        <tr>
-                          <th>Item </th>
-                          <th>Value</th>
-                          <th>Sender</th>
-                          <th>Sender's phone</th>
-                          <th>Sender's subscription</th>
-                          <th>Receiver</th>
-                          <th>Receiver's phone</th>
-                          <th>Receiver ID No</th>
-                          <th>Departure</th>
-                          <th>Departure date</th>
-                          <th>Departure time</th>
-                          <th>Destination</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        <tr>
-                        <?php foreach($dataProvider->getModels() as $item) {?>
-                          <tr>
-                          <td><?= $item->item_name ?></td>
-                         <td><?= $item->value ?></td>
-                         <td><?= $item->sender_name ?></td>
-                         <td><?= $item->sender_phone ?></td>
-                         <td><?= $item->sender_subscription ?></td>
-                         <td><?= $item->receiver_name ?></td>
-                         <td><?= $item->receiver_phone ?></td>
-                         <td><?= $item->receiver_id ?></td>
-                         <td><?= $item->departure ?></td>
-                         <td><?= $item->depature_date ?></td>
-                         <td><?= $item->departure_time ?></td>
-                         <td><?= $item->destination ?></td>
-                          </tr>
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
 
-<?php } ?>
-                        </tr>
-                      </tbody>
-                    </table>
-                  </div>
-                </div>
-              </div>
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
+</style>
+</head>
+<body>
+<h2>Items report</h2>
+<table>
+  <tr>
+      <th>Item </th>
+      <th>Value</th>
+      <th>Sender</th>
+      <th>Sender's phone</th>
+      <th>Sender's subscription</th>
+      <th>Receiver</th>
+      <th>Receiver's phone</th>
+      <th>Receiver ID No</th>
+      <th>Departure</th>
+      <th>Departure date</th>
+      <th>Departure time</th>
+      <th>Destination</th>
+  </tr>
+  <?php foreach ($dataProvider->getModels() as $item): ?>
+  <tr>
+  <td><?= $item->item_name ?></td>
+      <td><?= $item->value ?></td>
+      <td><?= $item->sender_name ?></td>
+      <td><?= $item->sender_phone ?></td>
+      <td><?= $item->sender_subscription ?></td>
+      <td><?= $item->receiver_name ?></td>
+      <td><?= $item->receiver_phone ?></td>
+      <td><?= $item->receiver_id ?></td>
+      <td><?= $item->departure ?></td>
+      <td><?= $item->depature_date ?></td>
+      <td><?= $item->departure_time ?></td>
+      <td><?= $item->destination ?></td>
+  </tr>
+  <?php endforeach; ?>
+</table>
+</body>
+</html>
+
+
 </div>
