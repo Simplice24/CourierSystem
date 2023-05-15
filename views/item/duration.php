e730862e9b59ef2b121a29077f4056b6e5ce0ce6
Simplice NIYONZIMA <89002414+Simplice24@users.noreply.github.com>
2023-05-15 11:00:58 -0800

    Profile

--- views/item/duration.php
+++ views/item/duration.php
@@ -16,6 +16,6 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="item-create">
 
     <h1><?= Html::encode($this->title) ?></h1>
-    <?= $this->render('_durationform') ?>
+    <?= $this->render('_durationform',['userProfileImage'=>$userProfileImage]) ?>
 
 </div>
